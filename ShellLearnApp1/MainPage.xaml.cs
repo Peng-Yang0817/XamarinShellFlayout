8a36a3584b29338e98cce111decfbc23b83cb71e
tcher2007 <tcher2007>
2023-01-18 23:30:02 +0800

    Flayout選單範本

---
+++ ShellLearnApp1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace ShellLearnApp1
+{
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneIcon_Clicked(object sender, EventArgs e)
+        {
+            // 調用系統撥號功能 Essentials
+            PhoneDialer.Open("0987587987");
+        }
+
+        private async void AboutIcon_Clicked(object sender, EventArgs e)
+        {
+            // 調用瀏覽器功能 Browser
+            await Browser.OpenAsync("https://www.pornhub.com/video/search?search=pronhub");
+        }
+    }
+}
